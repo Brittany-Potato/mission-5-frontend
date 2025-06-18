e62e78409a81338080264f7e4dacc38b54260ddc
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-19 11:40:46 +1200

    Setup basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import mainSearchPage from './pages/main-search-page';
+import productViewPage from './pages/product-view-page';
 import './App.css'
 
 function App() {
@@ -6,7 +9,12 @@ function App() {
 
   return (
     <>
-
+    <Router>
+    <Routes>
+      <Route path="/" element={<mainSearchPage />} />
+      <Route path="/" element={<productViewPage />} /> 
+    </Routes>
+    </Router>
     </>
   )
 }
