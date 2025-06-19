539802391aa97a38d10acd08ac0ac02ab825f111
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-19 21:06:14 +1200

    Started readme and added more folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import mainSearchPage from './pages/main-search-page';
 import productViewPage from './pages/product-view-page';
-import './App.css'
+import './App.css';
 
 function App() {
 
 
   return (
     <>
-    <Router>
-    <Routes>
-      <Route path="/" element={<mainSearchPage />} />
-      <Route path="/" element={<productViewPage />} /> 
-    </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<mainSearchPage />} />
+          <Route path="/" element={<productViewPage />} />
+        </Routes>
+      </Router>
     </>
   )
 }
