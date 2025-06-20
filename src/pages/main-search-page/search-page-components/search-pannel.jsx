735ab2be487b0538a0913ec58c2f838aff0aead9
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-20 13:58:07 +1200

    Finished first draft of the homepage search container CSS

--- src/pages/main-search-page/search-page-components/search-pannel.jsx
+++ src/pages/main-search-page/search-page-components/search-pannel.jsx
@@ -4,21 +4,31 @@ import styles from './search-pannel.module.css'
 export default function SearchPannel() {
     return (
         <div className={styles.mainSearchContainer}>
+            <div className={styles.searchButtonTopDiv}>
+                <button className={styles.searchTopButtons}>Home and Living</button>
+                <button className={styles.searchTopButtons}>Office</button>
+                <button className={styles.searchTopButtons}>Kitchen</button>
+                <button className={styles.searchTopButtons}>Outdoor/Garden</button>
+                <button className={styles.searchTopButtons}>All Catagories</button>
+            </div>
             {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
             <div className={styles.topSearchBoxes}>
-                <input type="select" />
-                <input type="select" />
-                <input type="select" />
+                <input type="select" placeholder='Search by'/>
+                <input type="select" placeholder='Location'/>
+                <input type="select" placeholder='Condition'/>
             </div>
             {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
             <div className={styles.middleSearchContainers}>
-                <input type="select" />
-                <input type="select" />
-                <input type="select" />
-                <input type="select" />
+                <input type="select" placeholder='Payment'/>
+                <input type="select" placeholder='Shipping'/>
+                <input type="select" placeholder='Price'/>
+                <input type="select" placeholder='Clearance'/>
             </div>
             <div className={styles.searchKeywordsDiv}>
-            <input type="search" />
+            <input type="search" placeholder='Search Keywords....'/>
+            </div>
+            <div className={styles.searchButtonDiv}>
+                <button className={styles.searchButton}>Search</button>
             </div>
         </div>
     )
