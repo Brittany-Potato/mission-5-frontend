61e5415a3f39e9aae3da08fa4908c67b78368798
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-20 14:21:44 +1200

    STARTED THE DIRECTORY UNDER THE SEARCH CONTAINER

--- src/pages/main-search-page/main-search-page.jsx
+++ src/pages/main-search-page/main-search-page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchPannel from './search-page-components/search-pannel.jsx';
 import styles from './main-search-page.module.css';
+import SearchDirectory from './search-page-components/search-directory.jsx';
 
 export default function MainSearchPage() {
   return (
     <div className={styles.mainSearchContainer}>
       <SearchPannel/>
+      <SearchDirectory/>
     </div>
   )
 }
---
+++ src/pages/main-search-page/search-page-components/search-directory.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from './search-directory.module.css'
+
+export default function SearchDirectory() {
+  return (
+    <div className={StyleSheet.mainSearchContainer}>
+        <ul>
+            <li className={styles.unorderedList}>
+                <a href="" className={styles.list}>Bathroom</a>
+                <a href="" className={styles.list}>Cleaning Bin</a>
+                <a href="" className={styles.list}>Curtains</a>
+                <a href="" className={styles.list}>Bedframe</a>
+                <a href="" className={styles.list}>Lamp</a>
+                <a href="" className={styles.list}>Laundry</a>
+                <a href="" className={styles.list}>Storage</a>
+                <a href="" className={styles.list}>Travel</a>
+                <a href="" className={styles.list}>Table</a>
+                <a href="" className={styles.list}>Kitchen</a>
+                <a href="" className={styles.list}>Heating and Cooling</a>
+                <a href="" className={styles.list}>Home Decor</a>
+                <a href="" className={styles.list}>Art Posters</a>
+                <a href="" className={styles.list}>Cleaning</a>
+                <a href="" className={styles.list}>Mat and Rugs</a>
+                <a href="" className={styles.list}>Outdoor</a>
+                <a href="" className={styles.list}>Clock</a>
+                <a href="" className={styles.list}>Utensils</a>
+                <a href="" className={styles.list}>Office</a>
+                <a href="" className={styles.list}>Blanket</a>
+            </li>
+        </ul>
+    </div>
+  )
+}
