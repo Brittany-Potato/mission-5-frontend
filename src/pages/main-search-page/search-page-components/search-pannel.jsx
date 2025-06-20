2493a5806b04659cb7cc5a4f138373a3d9318128
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-20 15:59:23 +1200

    Created an onClick function for the top search buttons on the home page

--- src/pages/main-search-page/search-page-components/search-pannel.jsx
+++ src/pages/main-search-page/search-page-components/search-pannel.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './search-pannel.module.css'
 
 export default function SearchPannel() {
+
+    const [selectButton, setSelectButton] = useState(false);
+
+    const buttons = [
+        { id: 'home', label: "Home and Living" },
+        { id: 'office', label: "Office" },
+        { id: 'kitchen', label: "Kitchen" },
+        { id: 'outdoor', label: "Outdoor/Garden" },
+        { id: 'all', label: "All Category" }
+    ]
+
+
+
+
+
+
     return (
         <div className={styles.mainSearchContainer}>
             <div className={styles.searchButtonTopDiv}>
-                <button className={styles.searchTopButtons}>Home and Living</button>
-                <button className={styles.searchTopButtons}>Office</button>
-                <button className={styles.searchTopButtons}>Kitchen</button>
-                <button className={styles.searchTopButtons}>Outdoor/Garden</button>
-                <button className={styles.searchTopButtons}>All Catagories</button>
+                {buttons.map(({ id, label }) => {
+                    return (
+                        <button
+                            key={id}
+                            className={`${styles.searchTopButtons} ${selectButton === id ? styles.clicked : ''}`} onClick={() => setSelectButton(id)}>{label}</button>
+                    )
+                })}
             </div>
             {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
             <div className={styles.topSearchBoxes}>
-                <input type="select" placeholder='Search by'/>
-                <input type="select" placeholder='Location'/>
-                <input type="select" placeholder='Condition'/>
+                <input type="select" placeholder='Search by' />
+                <input type="select" placeholder='Location' />
+                <input type="select" placeholder='Condition' />
             </div>
             {/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */}
             <div className={styles.middleSearchContainers}>
-                <input type="select" placeholder='Payment'/>
-                <input type="select" placeholder='Shipping'/>
-                <input type="select" placeholder='Price'/>
-                <input type="select" placeholder='Clearance'/>
+                <input type="select" placeholder='Payment' />
+                <input type="select" placeholder='Shipping' />
+                <input type="select" placeholder='Price' />
+                <input type="select" placeholder='Clearance' />
             </div>
             <div className={styles.searchKeywordsDiv}>
-            <input type="search" placeholder='Search Keywords....'/>
+                <input type="search" placeholder='Search Keywords....' className={styles.searchKeywords}/>
             </div>
             <div className={styles.searchButtonDiv}>
                 <button className={styles.searchButton}>Search</button>
