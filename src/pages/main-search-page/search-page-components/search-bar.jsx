0cd776151252bf11e405cd113deabf0e4becd377
brittany-potato <brittanycc@missionreadyhq.com>
2025-06-21 13:55:00 +1200

    grabbing information from the backend and displaying the response as an alert for the search bar on the homepage

--- src/pages/main-search-page/search-page-components/search-bar.jsx
+++ src/pages/main-search-page/search-page-components/search-bar.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './search-bar.module.css';
+import axios from 'axios';
 
 export default function SearchBar() {
+
+  const [value, setValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
+
+  const handleKeyDown = async (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      try {
+        const response = await axios.post('http://localhost:3000/homepageSearch', {
+          search: inputValue
+        });
+        const innerData = response.data;
+
+        Object.entries(innerData).forEach(([key, value]) => {
+          if (typeof value === 'object' && value !== null) {
+            alert(`${key}: ${JSON.stringify(value, null, 2)}`);
+          } else {
+            alert(`${key}: ${value}`);
+          }
+        });
+        // alert(response.data);
+      } catch (err) {
+        console.error("Failed to search:", err.message);
+      }
+    };
+  }
+  //   const handleKey = () => {
+  //   console.log("You entered:", value);
+  // }
+
+
+
+
+
+
   return (
     <div className={styles.searchBarContainer}>
-        <input type="search" placeholder='Search all...' className={styles.searchBar}/>
+      <input type="text" placeholder='Search all...' className={styles.searchBar}
+        onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyDown}
+        value={inputValue} />
     </div>
   )
 }
