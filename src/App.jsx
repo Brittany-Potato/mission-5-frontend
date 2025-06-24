4ea0887d4c5c42a2e97a8bf24da7f3a10872d7a0
Afton Lim <aftonl@missionreadyhq.com>
2025-06-24 17:04:07 +1200

    Fixed routes, page now displaying

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainSearchPage from './pages/main-search-page/main-search-page';
-// import productViewPage from './pages/product-view-page/product-view-page';
+import { ProductViewPage } from './pages/product-view-page/product-view-page';
 import './App.module.css';
 
 function App() {
-
-
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainSearchPage />} />
-          {/* <Route path="/" element={<productViewPage />} /> */}
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainSearchPage />} />
+        <Route path="/product/:id" element={<ProductViewPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
